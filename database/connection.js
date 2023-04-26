477723bb76ab9761ac6936772b437e4791fb0e8b
Fahad Rajput <fahadameenrajput@gmail.com>
2023-04-26 23:46:06 +0500

    Updated DB String

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config({ path: './config.env' });
-const database = "<Your Data Base String Here></Your>";
+const database = "mongodb+srv://sp19bse036:UKeVdGP8JHT5zU5R@tiresite.2sdw1jz.mongodb.net/?retryWrites=true&w=majority&ssl=true";
 
 mongoose.connect(database,{
     useNewUrlParser :true,
