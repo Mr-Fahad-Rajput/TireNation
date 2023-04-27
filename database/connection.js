920cd260d4948e4d1b4a60d36c1f6859587dc57e
Fahad Rajput <fahadameenrajput@gmail.com>
2023-04-27 19:45:06 +0500

    updated Database

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config({ path: './config.env' });
-const database = "mongodb+srv://sp19bse036:UKeVdGP8JHT5zU5R@tiresite.2sdw1jz.mongodb.net/?retryWrites=true&w=majority&ssl=true";
+const database = "mongodb+srv://inhousexperts:NG3fPIiz98is8Vfx@tirenation.vne8nxr.mongodb.net/?retryWrites=true&w=majority&ssl=true";
 
 mongoose.connect(database,{
     useNewUrlParser :true,
